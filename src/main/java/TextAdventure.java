94062949d585b96ac3471e1a67479a99c128d089
Lizzie314 <elizabethbippley@gmail.com>
2025-10-27 23:45:07 -0400

    Update TextAdventure.java

--- src/main/java/TextAdventure.java
+++ src/main/java/TextAdventure.java
@@ -8,24 +8,46 @@ public class TextAdventure
         Scanner sc = new Scanner(System.in);
 
         // Declare Variables
-        boolean selectOption = true;
-        int gameOption;
+        boolean playing = true;
+        String gameOption;
+        String gameMode;
 
-        // Welcome Screen
-        while (selectOption)
+        // Game
+        while (playing)
         {
+            // Welcome Screen
             System.out.println("Welcome to Elf Enchanted");
             System.out.println("Please select an option: ");
             System.out.println("1. Play");
             System.out.println("2. Game Premise");
             System.out.println("3. Exit");
-            gameOption = sc.nextInt();
-        }
+            gameOption = sc.nextLine();
 
-        // Game Mode Selection
-        System.out.println("Please Choose a Game Mode: ");
-        System.out.println("1. Crystal Mountain");
-        System.out.println("2. Magic Forest");
-        System.out.println("3. City Quest");
+            if (gameOption.equals("1"))
+            {
+                // Game Mode Selection
+                System.out.println("Please Choose a Game Mode: ");
+                System.out.println("1. Crystal Mountain");
+                System.out.println("2. Magic Forest");
+                System.out.println("3. City Quest");
+                gameMode = sc.nextLine();
+            }
+
+            else if (gameOption.equals("2"))
+            {
+
+            }
+
+            else if (gameOption.equals("3"))
+            {
+                System.out.println("Goodbye! Hope you had fun today!");
+                playing = false;
+            }
+
+            else
+            {
+                System.out.println("Please enter a valid choice");
+            }
+        }
     }
 }
