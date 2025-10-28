57f28a5100cdb873308826b417813ebe4fb1df84
Lizzie314 <elizabethbippley@gmail.com>
2025-10-28 13:57:24 -0400

    Update TextAdventure.java

--- src/main/java/TextAdventure.java
+++ src/main/java/TextAdventure.java
@@ -11,6 +11,7 @@ public class TextAdventure
         boolean playing = true;
         String gameOption;
         String gameMode;
+        boolean selecting = true;
 
         // Game
         while (playing)
@@ -25,12 +26,41 @@ public class TextAdventure
 
             if (gameOption.equals("1"))
             {
-                // Game Mode Selection
-                System.out.println("Please Choose a Game Mode: ");
-                System.out.println("1. Crystal Mountain");
-                System.out.println("2. Magic Forest");
-                System.out.println("3. City Quest");
-                gameMode = sc.nextLine();
+                while (selecting)
+                {
+                    // Game Mode Selection
+                    System.out.println("Please Choose a Game Mode: ");
+                    System.out.println("1. Crystal Mountain");
+                    System.out.println("2. Magic Forest");
+                    System.out.println("3. City Quest");
+                    gameMode = sc.nextLine();
+
+                    if (gameMode.equals("1"))
+                    {
+                        System.out.println("Welcome to Crystal Mountain");
+                        System.out.println("You have been sent on a mission to collect various crystals around the");
+                        System.out.println("mountain.  These crystals hold varying powers that can be used for evil if");
+                        System.out.println("they are put in the wrong hands.  Unfortunately, there is a villianous group");
+                        System.out.println("also racing to try to find some of the crystals before you get to them.");
+                        playing = false;
+                        selecting = false;
+                    }
+
+                    else if (gameMode.equals("2"))
+                    {
+
+                    }
+
+                    else if (gameMode.equals("3"))
+                    {
+
+                    }
+
+                    else
+                    {
+                        System.out.println("Please enter a valid choice");
+                    }
+                }
             }
 
             else if (gameOption.equals("2"))
